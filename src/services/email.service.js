66de67d8286f0f6a16aa6bc44b6d93664ba99770
lockrr <hemistergt@gmail.com>
2024-06-15 11:05:07 +0700

    add await

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -20,24 +20,26 @@ if (config.env !== 'test') {
  */
 const sendEmail = async (to, subject, text) => {
   const msg = { from: config.email.from, to, subject, text };
-  await transport.sendMail(msg);
+  await new Promise((resolve, reject) => {
+    transport.sendMail(msg);
+  });
 };
 
-/**
- * Send reset password email
- * @param {string} to
- * @param {string} token
- * @returns {Promise}
- */
-const sendResetPasswordEmail = async (to, token) => {
-  const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
-  await sendEmail(to, subject, text);
-};
+// /**
+//  * Send reset password email
+//  * @param {string} to
+//  * @param {string} token
+//  * @returns {Promise}
+//  */
+// const sendResetPasswordEmail = async (to, token) => {
+//   const subject = 'Reset password';
+//   // replace this url with the link to the reset password page of your front-end app
+//   const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+//   const text = `Dear user,
+// To reset your password, click on this link: ${resetPasswordUrl}
+// If you did not request any password resets, then ignore this email.`;
+//   await sendEmail(to, subject, text);
+// };
 
 // /**
 //  * Send verification email
