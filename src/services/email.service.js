477e4543f17e1d0ed6b536ca490f4313199bf54c
lockrr <hemistergt@gmail.com>
2024-06-28 23:32:07 +0700

    fix url

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -33,7 +33,7 @@ const sendEmail = async (to, subject, text) => {
 
   const emailConfig = {
     method: 'post',
-    url: 'https://studio-foto-backend.vercel.app/v1/emails',
+    url: 'https://api.resend.com/emails',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${config.email.smtp.auth.pass}`,
@@ -68,7 +68,7 @@ If you did not request any password resets, then ignore this email.`;
 const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
+  const verificationEmailUrl = `https://studio-foto-backend.vercel.app/v1/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
